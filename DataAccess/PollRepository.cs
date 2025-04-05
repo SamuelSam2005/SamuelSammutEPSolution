14dec45b8ce52140db6b37337afc1d20234665e1
Samuel Sammut <samuel.sammut.i79778@mcast.edu.mt>
2025-04-05 13:58:16 +0200

    Implemented full CRUD functionality in PollController and updated Create, Edit, and Index views with Bootstrap and proper formatting

--- DataAccess/PollRepository.cs
+++ DataAccess/PollRepository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DataAccess;
 using Domain;
-
+using Domain;
 
 namespace DataAccess
 {
@@ -18,6 +15,7 @@ namespace DataAccess
             _context = context;
         }
 
+        // Already done
         public void CreatePoll(string title, string option1Text, string option2Text, string option3Text)
         {
             var poll = new Poll
@@ -35,6 +33,37 @@ namespace DataAccess
             _context.Polls.Add(poll);
             _context.SaveChanges();
         }
+
+        // ✅ Get all polls, sorted by date (LINQ)
+        public List<Poll> GetAllPolls()
+        {
+            return _context.Polls
+                .OrderByDescending(p => p.DateCreated)
+                .ToList();
+        }
+
+        // ✅ Find poll by ID
+        public Poll? GetPollById(int id)
+        {
+            return _context.Polls.FirstOrDefault(p => p.Id == id);
+        }
+
+        // ✅ Update poll
+        public void UpdatePoll(Poll updatedPoll)
+        {
+            _context.Polls.Update(updatedPoll);
+            _context.SaveChanges();
+        }
+
+        // ✅ Delete poll
+        public void DeletePoll(int id)
+        {
+            var poll = _context.Polls.FirstOrDefault(p => p.Id == id);
+            if (poll != null)
+            {
+                _context.Polls.Remove(poll);
+                _context.SaveChanges();
+            }
+        }
     }
 }
-
--- Presentation/Controllers/PollController.cs
+++ Presentation/Controllers/PollController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
-using DataAccess;
+
 
 namespace Presentation.Controllers
 {
@@ -14,26 +15,62 @@ namespace Presentation.Controllers
             _pollRepository = pollRepository;
         }
 
-        // Show Create Form
+        // ✅ GET: Create Poll Form
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // Handle Form Submission
+        // ✅ POST: Create Poll using Method Injection
         [HttpPost]
         public IActionResult Create(string title, string option1, string option2, string option3)
         {
-            // ✅ Method Injection: inject via parameter
             CreatePoll(_pollRepository, title, option1, option2, option3);
-            return RedirectToAction("Index"); // We'll implement Index next step
+            return RedirectToAction("Index");
         }
 
         // ✅ Method Injection
         private void CreatePoll(PollRepository repo, string title, string o1, string o2, string o3)
         {
             repo.CreatePoll(title, o1, o2, o3);
+        }
+
+        // ✅ READ: Show all polls
+        public IActionResult Index()
+        {
+            var polls = _pollRepository.GetAllPolls();
+            return View(polls);
+        }
+
+        // ✅ GET: Edit poll by id
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var poll = _pollRepository.GetPollById(id);
+            if (poll == null)
+                return NotFound();
+
+            return View(poll);
+        }
+
+        // ✅ POST: Save edited poll
+        [HttpPost]
+        public IActionResult Edit(Poll poll)
+        {
+            if (!ModelState.IsValid)
+                return View(poll);
+
+            _pollRepository.UpdatePoll(poll);
+            return RedirectToAction("Index");
+        }
+
+        // ✅ POST: Delete poll
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _pollRepository.DeletePoll(id);
+            return RedirectToAction("Index");
         }
     }
 }
